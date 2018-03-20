34b7306602fbd3c9d37e9e401247ca2d4b24622a
Unknown <drfavor@gmail.com>
2018-03-20 18:15:49 -0500

    song submit

--- getInfo.php
+++ getInfo.php
@@ -1,7 +1,12 @@
 <html>
 <body>
-
-Welcome <?php echo $_POST['name']; ?><br>
-
+<?php
+	$message = "Name: " . $_POST['name'] . "\nSong: " . $_POST['song'] . "\nArtist: " . $_POST['artist'];
+	mail("kteebaldus@gmail.com","song suggestions",$message);
+?>
+submitted successfully!<br>
+Name: <?php echo $_POST['name']; ?><br>
+Song: <?php echo $_POST['song']; ?><br>
+artist: <?php echo $_POST['artist']; ?><br>
 </body>
 </html>
